c67121d1e806131861b95469950960799e66b076
zlendo1 <zlendo1@etf.unsa.ba>
2024-08-08 13:46:03 +0200

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
 import Login from './components/Login/Login.jsx';
 import Register from './components/Register/Register.jsx';
+import Home from "./components/Home/Home.jsx";
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const jwtToken = Cookies.getItem('jwt');
+
+        setIsLoggedIn(!!jwtToken);
+    }, []);
+
     return (
-        <div className="App">
-            <Login />
-            <Register />
-        </div>
+        <Router basename='/'>
+            <Routes>
+                <Route path="/login" element={ isLoggedIn ? <Home /> : <Login /> } />
+                <Route path="/register" element={ isLoggedIn ? <Home /> : <Register /> } />
+                <Route path="/" element={ isLoggedIn ? <Home /> : <Login /> } />
+            </Routes>
+        </Router>
     );
 }
 
