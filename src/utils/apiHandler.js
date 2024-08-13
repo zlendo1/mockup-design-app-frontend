fa9cb50604b5ec39cbd695d03545f14ed363a985
zlendo1 <zlendo1@etf.unsa.ba>
2024-08-13 19:11:06 +0200

    added api handler to utils

---
+++ src/utils/apiHandler.js
@@ -0,0 +1,48 @@
+async function handleResponse(response) {
+	if (response.ok) {
+		return response.json();
+	} else {
+		const data = await response.json();
+
+		throw new Error(data.message);
+	}
+}
+
+/**
+ * Makes a POST request to the specified URL with the given request body.
+ *
+ * @param {string} url - The URL to make the POST request to.
+ * @param {Object} requestBody - The body of the POST request.
+ * @returns {Promise<Object>} - A promise that resolves to the JSON data from the response.
+ */
+function callPost(url, requestBody) {
+	return fetch(import.meta.env.BACKEND_URL + url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(requestBody)
+	})
+	.then(async response => handleResponse(response))
+}
+
+/**
+ * Makes a GET request to the specified URL.
+ *
+ * @param {string} url - The URL to make the GET request to.
+ * @returns {Promise<Object>} - A promise that resolves to the JSON data from the response.
+ */
+function callGet(url) {
+	return fetch(import.meta.env.BACKEND_URL + url, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	})
+	.then(async response => handleResponse(response))
+}
+
+export default {
+	callPost,
+	callGet
+};
