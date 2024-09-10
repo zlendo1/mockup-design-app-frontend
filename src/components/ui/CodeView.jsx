59133997be6c7a9dc8ab00e095e15bea3c47c5b4
zlendo1 <zlendo1@etf.unsa.ba>
2024-09-11 00:19:54 +0200

    implemented Drawer and CodeView for code export

---
+++ src/components/ui/CodeView.jsx
@@ -0,0 +1,21 @@
+import { CopyBlock, dracula } from 'react-code-blocks'
+import PropTypes from 'prop-types'
+
+const CodeView = ({ codeString }) => {
+	return (
+		<div className="h-full rounded-md border border-input">
+			<CopyBlock
+				customStyle={{ height: '100%', overflow: 'scroll' }}
+				text={codeString}
+				language={'javascript'}
+				theme={dracula}
+			/>
+		</div>
+	)
+}
+
+CodeView.propTypes = {
+	codeString: PropTypes.string.isRequired,
+}
+
+export default CodeView
