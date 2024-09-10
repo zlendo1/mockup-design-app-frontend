b76ae83a908419a5d183e6c01cec4b3a8b53bc9d
zlendo1 <zlendo1@etf.unsa.ba>
2024-09-10 18:40:27 +0200

    added workspace functionality

--- src/components/Home/MainContent/ProjectEditor/Workspace.jsx
+++ src/components/Home/MainContent/ProjectEditor/Workspace.jsx
@@ -1,25 +1,42 @@
 import PropTypes from 'prop-types'
-import { useState } from 'react'
+import { createContext, useState } from 'react'
+import { Editor, Frame, Element } from '@craftjs/core'
 
 import LeftSidebar from './LeftSidebar/LeftSidebar.jsx'
 import RightSidebar from './RightSidebar/RightSidebar.jsx'
 import Canvas from './Canvas/Canvas.jsx'
+import Viewport from './Canvas/Viewport.jsx'
+
+// eslint-disable-next-line no-unused-vars
+export const SaveContext = createContext(_ => {})
 
 const Workspace = ({ project, onSave }) => {
 	const [width, setWidth] = useState('') // State for width
 	const [height, setHeight] = useState('') // State for height
 
 	return (
-		<div className="flex flex-grow">
-			<LeftSidebar projectName={project.name} />
-			<Canvas onSave={onSave} />
-			<RightSidebar
-				height={height}
-				width={width}
-				setHeight={setHeight}
-				setWidth={setWidth}
-			/>
-		</div>
+		<SaveContext.Provider value={onSave}>
+			<Editor
+				resolver={{
+					Canvas,
+				}}
+			>
+				<div className="relative flex flex-1 overflow-hidden">
+					<LeftSidebar projectName={project.name} />
+					<Viewport>
+						<Frame>
+							<Element is={Canvas} id="ROOT" canvas></Element>
+						</Frame>
+					</Viewport>
+					<RightSidebar
+						height={height}
+						width={width}
+						setHeight={setHeight}
+						setWidth={setWidth}
+					/>
+				</div>
+			</Editor>
+		</SaveContext.Provider>
 	)
 }
 
