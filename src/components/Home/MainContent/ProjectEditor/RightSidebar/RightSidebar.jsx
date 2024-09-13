bf68a8647f79616d0712f8dc60f6b4798f01b027
zlendo1 <zlendo1@etf.unsa.ba>
2024-09-13 20:56:52 +0200

    moved styling and hierarchy back to tabs

--- src/components/Home/MainContent/ProjectEditor/RightSidebar/RightSidebar.jsx
+++ src/components/Home/MainContent/ProjectEditor/RightSidebar/RightSidebar.jsx
@@ -1,24 +1,36 @@
 import Hierarchy from './Hierarchy/Hierarchy.jsx'
 import Styling from './Styling/Styling.jsx'
+import { useState } from 'react'
+import { classMerger } from '@/utils/cssClassHandler.js'
 
 const RightSidebar = () => {
+	const [activeTab, setActiveTab] = useState('styling')
+
 	return (
 		<aside className="flex w-80 flex-col border-l bg-white">
-			<div className="h-1/2 w-full border-b">
-				<h3 className="text-md border-b px-4 py-2 text-left font-semibold">
-					Hierarchy
-				</h3>
-				<div>
-					<Hierarchy />
-				</div>
-			</div>
-			<div className="h-1/2 w-full">
-				<h3 className="text-md border-b px-4 py-2 text-left font-semibold">
+			<div className="flex w-full">
+				<button
+					className={classMerger(
+						'text-md w-1/2 border-b bg-gray-200 px-4 py-2 text-left font-semibold',
+						activeTab === 'styling' && 'bg-white'
+					)}
+					onClick={() => setActiveTab('styling')}
+				>
 					Styling
-				</h3>
-				<div className="p-4">
-					<Styling />
-				</div>
+				</button>
+				<button
+					className={classMerger(
+						'text-md w-1/2 border-b border-l bg-gray-200 px-4 py-2 text-left font-semibold',
+						activeTab === 'hierarchy' && 'bg-white'
+					)}
+					onClick={() => setActiveTab('hierarchy')}
+				>
+					Hierarchy
+				</button>
+			</div>
+			<div className="p-2">
+				{activeTab === 'styling' && <Styling />}
+				{activeTab === 'hierarchy' && <Hierarchy />}
 			</div>
 		</aside>
 	)
