60f5d2d42660ce2f9e43c89ce121f0eaf1fba67d
zlendo1 <zlendo1@etf.unsa.ba>
2024-09-13 17:50:38 +0200

    better formatted header

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -2,22 +2,22 @@ import PropTypes from 'prop-types'
 
 const Header = ({ username, onLogout, onProjectSelect }) => {
 	return (
-		<header className="flex items-center justify-between bg-gray-200 p-4">
-			<div className="flex space-x-4">
-				<span className="text-lg font-semibold">
+		<header className="flex items-center justify-between border-b bg-white p-4">
+			<div className="flex items-center space-x-4">
+				<span className="text-lg font-semibold text-black">
 					Welcome, {username}
 				</span>
 			</div>
 			<div className="flex space-x-4">
 				<button
 					onClick={onProjectSelect}
-					className="rounded bg-blue-500 px-4 py-2 text-white"
+					className="rounded bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
 				>
 					Select Project
 				</button>
 				<button
 					onClick={onLogout}
-					className="rounded bg-red-500 px-4 py-2 text-white"
+					className="rounded bg-red-500 px-4 py-2 text-white transition duration-300 hover:bg-red-600"
 				>
 					Logout
 				</button>
